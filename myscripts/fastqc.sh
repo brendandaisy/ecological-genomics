ca6bd4790c62f751b62b763dd964952cf7250903
Brendan K Case <bcase@pbio381.uvm.edu>
2020-01-30 10:36:06 -0500

    added trim func

--- myscripts/fastqc.sh
+++ myscripts/fastqc.sh
@@ -2,13 +2,10 @@
 
 cd ~/ecological-genomics
 
-mkdir fastqc
+mkdir fastqc # makes dir if not already exists
 
 for f in /data/project_data/RS_ExomeSeq/fastq/edge_fastq/PRK*
-
 do
-
-fastqc ${f} -o fastqc/
-
+    fastqc ${f} -o fastqc/
 done
 
